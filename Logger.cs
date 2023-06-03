f0fd7a782955cd36a48e8e053ef21438a72eea98
noam79 <noameilon8@gmail.com>
2023-06-03 21:05:41 +0300

    Initial commit

---
+++ Logger.cs
@@ -0,0 +1,18 @@
+﻿namespace MultiLogger;
+
+public static class Logger
+{
+    public const string LogPath = "log.txt";
+
+    public static void WriteLine(string log)
+    {
+        Console.WriteLine(DateTime.Now + log);
+        File.AppendAllText(LogPath, DateTime.Now +log + '\n');
+    }
+
+    public static void Write(string log)
+    {
+        Console.Write(DateTime.Now + log);
+        File.AppendAllText(LogPath, DateTime.Now + log);
+    }
+}
